220dc7ea2864fd829d9ecd01bd3b0bb0ab44f167
秀俊 笹島 <230259@jc-21.jp>
2024-12-05 11:34:58 +0900

    deltaTimeのGetDeltaTimeにコメントを追加

--- Engine/DeltaTime.h
+++ Engine/DeltaTime.h
@@ -3,6 +3,11 @@ namespace DeltaTime
 {
 	void Initialize();
 	void Update();
+	/// <summary>
+	/// フレーム間の時間を返す関数
+	/// </summary>
+	/// フレーム間の時間を秒単位で返す
+	/// <returns></returns>
 	double GetDelaTime();
 };
 
