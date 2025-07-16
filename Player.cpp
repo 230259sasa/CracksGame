d570c0373d1f4f81f066e0efe58655321bc41449
230259 <230259@E0404-13>
2025-07-16 11:09:45 +0900

    定数化

--- Player.cpp
+++ Player.cpp
@@ -21,9 +21,12 @@ namespace PlayerSet {
 	//const float FALL_CORRECTION_Y(0.1f);//落下時のRayCastの開始座標Yに足す補正値
 	const XMFLOAT3 FORWARD_VECTOR(0, 0, 1);//前方へのベクトル
 
+	const int DEGREES_360(360);
 	const int LEFT_MOVE_ANGLE(90);
 	const int BACK_MOVE_ANGLE(180);
 	const int RIGHT_MOVE_ANGLE(270);
+	const int NUM_SECTORS(8);
+	const int SECTOR_ANGLE(DEGREES_360 / NUM_SECTORS);
 	float PLAYER_RADIUS(0);
 	float MOVE_SPEED(0);
 	float JUMP_HEIGHT(0);//ジャンプの高さ
@@ -143,8 +146,7 @@ void Player::AnimationManager()
 
 bool Player::Move()
 {
-	int angle = Camera::GetRotateAngle() % 360;
-	if (angle < 0) angle += 360;
+	int angle = Camera::GetRotateAngle() % Set::DEGREES_360;
 
 	//ラジアンに変換
 	float rad = XMConvertToRadians(angle);
@@ -236,10 +238,10 @@ void Player::Fall()
 	stage = (Stage*)FindObject("Stage");
 	if (stage != nullptr) {
 		float radius = Set::FALL_RAY_CAST_RADIUS;
-		for (int i = 0; i < 8; i++) {
+		for (int i = 0; i < Set::NUM_SECTORS; i++) {
 			RayCastData rayData;
 			XMFLOAT3 pos = transform_.position_;
-			float r = XMConvertToRadians(45*i);
+			float r = XMConvertToRadians(Set::SECTOR_ANGLE*i);
 			float x = 0 * cos(r) - radius * sin(r);
 			float z = 0 * sin(r) + radius * cos(r);
 			rayData.start = { pos.x + x,pos.y,pos.z + z,0.0f };
@@ -303,16 +305,16 @@ void Player::BreakStageBlock()
 
 	int x, y, z;
 	x = (int)(transform_.position_.x);
-	y = (int)(transform_.position_.y) - 1;
+	y = (int)(transform_.position_.y);
 	z = (int)(transform_.position_.z);
 
-	int angle = ((int)transform_.rotate_.y % 360);
+	int angle = ((int)transform_.rotate_.y % Set::DEGREES_360);
 	if (angle < 0)
-		angle = 360 + angle;
-	int dir = angle / 45;
+		angle = Set::DEGREES_360 + angle;
+	int dir = angle / Set::SECTOR_ANGLE;
 	
-	int arrX[8] = { 0,1,1,0,0,-1,-1,0 };
-	int arrZ[8] = { 1,0,0,-1,-1,0,0,1 };
+	int arrX[Set::NUM_SECTORS] = { 0,1,1,0,0,-1,-1,0 };
+	int arrZ[Set::NUM_SECTORS] = { 1,0,0,-1,-1,0,0,1 };
 
 	x = (int)(transform_.position_.x) + arrX[dir];
 	z = (int)(transform_.position_.z) + arrZ[dir];
