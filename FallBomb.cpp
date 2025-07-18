7e96847249771a4ad04e792b7f24a6e2abf42fdb
230259 <230259@E0404-13>
2025-07-18 16:33:27 +0900

    爆発の破壊の実装中

--- FallBomb.cpp
+++ FallBomb.cpp
@@ -1,6 +1,7 @@
 #include "FallBomb.h"
 #include "Engine\Model.h"
 #include "Engine\JsonReader.h"
+#include "Stage.h"
 #include "Timer.h"
 
 namespace JR = JsonReader;
@@ -28,15 +29,34 @@ void FallBomb::Release()
 void FallBomb::Update()
 {
 	Fall();
+	Explosion();
 	Dead();
 }
 
 void FallBomb::Explosion()
 {
-	if (!IsOnGround())
+	if (!IsOnGround()) {
+		explosionTimer_->StopTimer();
 		return;
+	}
+	else {
+		explosionTimer_->StartTimer();
+	}
 	
 	if (explosionTimer_->IsTimeOver(JR::Get<float>(objectName_, "EXPLOSION_TIME"))) {
+		Stage* stage = (Stage*)FindObject("Stage");
+		if (stage == nullptr)
+			return;
 		explosionTimer_->ResetTimer();
+		XMINT3 pos = {	(int)transform_.position_.x,
+						(int)transform_.position_.y,
+						(int)transform_.position_.z };
+		for (int x = -1;x < 2;x++) {
+			for (int y = -1;y < 2;y++) {
+				for (int z = -1;z < 2;z++) {
+					stage->SetFallBlock(pos.x + x, pos.y + y, pos.z + z);
+				}
+			}
+		}
 	}
 }
